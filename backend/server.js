ad7d5b9f4293d0b1458baf16dbf25e1422a64da4
krishna <68892307+krishnabhatta01@users.noreply.github.com>
2023-08-21 10:38:23 +0545

    first commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+app.get('/',(req,res) => {
+    res.send('hello world')
+})
+
+app.listen(port,() => {
+    console.log(`Listening on port ${port}`)
+})
+
