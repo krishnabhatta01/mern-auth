c580d817316bbe0baa4509e0e895a749411703c2
krishna <68892307+krishnabhatta01@users.noreply.github.com>
2023-08-22 11:01:11 +0545

    custom middleware created

---
+++ backend/controllers/userController.js
@@ -0,0 +1,9 @@
+import asyncHandler from "express-async-handler";
+
+const authUser = asyncHandler(async  (req,res) => {
+    res.status(401);
+    throw new Error('Something went wrong');
+    res.status(200).json({message: "Auth user"});
+});
+
+export {authUser};
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 import express from "express";
 import dotenv from 'dotenv';
+import userRoute from './routes/userRoute.js';
+import {errorHandler, notFound} from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use('/api/users',userRoute)
+app.use(notFound);
+app.use(errorHandler);
+
 
 app.get('/',(req,res) => {
     res.send('hello world')
 })
+
 
 app.listen(port,() => {
     console.log(`Listening on port ${port}`)
