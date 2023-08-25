026e81f77632e99fae327f9c67605dbb0d31760b
krishna <68892307+krishnabhatta01@users.noreply.github.com>
2023-08-26 00:00:17 +0545

    mongoDB database setup

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,17 @@ import express from "express";
 import dotenv from 'dotenv';
 import userRoute from './routes/userRoute.js';
 import {errorHandler, notFound} from './middleware/errorMiddleware.js';
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const app = express();
+connectDB();
 
 app.use('/api/users',userRoute)
 app.use(notFound);
 app.use(errorHandler);
-
-
-app.get('/',(req,res) => {
-    res.send('hello world')
-})
 
 
 app.listen(port,() => {
