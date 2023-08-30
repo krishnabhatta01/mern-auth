dfbb2301c3ee523dd2b84814a1a5d9fb20a9ece2
krishna <68892307+krishnabhatta01@users.noreply.github.com>
2023-08-30 23:40:20 +0545

    minor error fixed

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-const generateToken = (req, res, userId) => {
+const generateToken = ( res, userId) => {
     const token = jwt.sign({ userId} , process.env.JWT_SECRET,{
         expiresIn:'30d',
     });
@@ -9,7 +9,7 @@ const generateToken = (req, res, userId) => {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
         sameSite: 'strict',
-        maxAge:86400
+        maxAge:86400000,
     })
 };
 
