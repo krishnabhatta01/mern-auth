1ffc9f4c9c5af01d51c6b06124f1ac5e592d4991
krishna <68892307+krishnabhatta01@users.noreply.github.com>
2023-08-31 00:25:18 +0545

    logout endpoint completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,7 +48,11 @@ const registerUser = asyncHandler(async  (req,res) => {
 
 // @access  private
 const logoutUser = asyncHandler(async  (req,res) => {
-    res.status(200).json({message: "logout user"});
+    res.cookie('jwt', '', { 
+        httpOnly: true,
+        expires: new Date(0) 
+    });
+    res.status(200).json({message:'User logged out'});
 });
 
 // @access  private
