de448ddf737ff685bafde60bff307098146c8a16
krishna <68892307+krishnabhatta01@users.noreply.github.com>
2023-09-05 00:51:00 +0545

    cors error fixed

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,14 @@ import {errorHandler, notFound} from './middleware/errorMiddleware.js';
 import connectDB from "./config/db.js";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
 app.use(cookieParser());
 
 // app.use(express.json());
